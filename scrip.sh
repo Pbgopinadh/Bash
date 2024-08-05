52c49154b666a431e5b3e42a5e920311e8c0b493
Perumalla Bogeswar Gopinadh <perumallabogeswargopinadh@gmail.com>
2024-08-05 19:39:38 +0530

    added scrip.sh

---
+++ scrip.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+name=$1
+Age = $2
+
+echo -e "Hi!, My name is $(name) Nice to meet you \nmy age is $(age)"
+echo "$(whoami) is the one executing this $0 script".
+echo "the arguments are $*"
+echo "the arguments are $#"
+
+echo -e "\e[32m this is a warning [0m"
+
+
