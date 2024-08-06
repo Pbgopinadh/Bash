ba7cbb8cb830f33680fe663ede03a84276ce242a
Perumalla Bogeswar Gopinadh <perumallabogeswargopinadh@gmail.com>
2024-08-06 23:58:55 +0530

    added readinputfromuser.sh

--- conditions.sh
+++ conditions.sh
@@ -2,29 +2,29 @@
 
 number=$1
 
-if [$number = 0] ; then 
+if [ $number = 0 ] ; then 
 
-echo "shutting down the system"
+    echo "shutting down the system"
 
-elif [$number = 1] ; then 
+elif [ $number = 1 ] ; then 
 
-echo "starting the system"
+    echo "starting the system"
 
-elif [$number = 2] ; then 
+elif [ $number = 2 ] ; then 
 
-echo "safe mode"
+    echo "safe mode"
 
-elif [$number = 3] ; then
+elif [ $number = 3 ] ; then
 
-echo "GUI mode"
+    echo "GUI mode"
 
-elif [$number = 4] ; then
+elif [ $number = 4 ] ; then
 
-echo "undefined mode for debugging"
+    echo "undefined mode for debugging"
 
 else 
 
-echo "enter a valid option from 0 to 4"
+    echo "enter a valid option from 0 to 4"
 
 fi # this is how to mention the end of the condition if - start fi - end.
 
