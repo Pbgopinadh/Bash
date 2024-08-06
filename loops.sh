ea4f59c217563f8b820b4dabe46813d541dcd8be
Perumalla Bogeswar Gopinadh <perumallabogeswargopinadh@gmail.com>
2024-08-07 01:44:11 +0530

    loops

--- loops.sh
+++ loops.sh
@@ -5,16 +5,16 @@
 # variation of using for loop
 
 for i in 1 2 3 4 5 6 
+do #do - start of the expression space
+    echo "the current value of i is $i"
+done #done - end of the expression space
+
+for i in {7..20} # instead of providing the numbers manually the syntax is 1..5 means 1 to 5
 do
     echo "the current value of i is $i"
 done
 
-for i in {7..20}
-do
-    echo "the current value of i is $i"
-done
-
-for i in /home/ec2-user/*
+for i in /home/ec2-user/* # this is how to iterate though a directory path
 do
     echo "the current directory is $i"
 done
